f65e14bedeab92eb7d0cc6bdc64bccae2711b3e3
Shuxiao <shuxiao_song@outlook.com>
2024-09-12 23:32:48 +0800

    Task 4-2

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,8 +1,9 @@
 public class Multiples {
     public static void main(String[] args) {
-        int a = 1000/3;
-        int b = 1000/5;
-        int c = 1000/(3*5);
+        int k = 1000 - 1;
+        int a = k/3;
+        int b = k/5;
+        int c = k/(3*5);
         int d = a + b - c;
         System.out.println(d);
     }
