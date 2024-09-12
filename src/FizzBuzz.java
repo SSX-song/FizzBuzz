99be10335842e0388d5fb6069400ecbabd1d541a
Shuxiao <shuxiao_song@outlook.com>
2024-09-12 23:22:00 +0800

    Task 3

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -30,5 +30,36 @@ class FizzBuzz {
 
             }
         }
+        int a = 1;
+        while (a < 100) {
+            a = doFizzBuzz(a);
+        }
+    }
+
+    public static int doFizzBuzz(int a) {
+        boolean divisibleBy3 = a % 3 == 0;
+        boolean divisibleBy5 = a % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(a);
+        }
+        a = a + 1;
+        return a;
     }
 }
+
+
