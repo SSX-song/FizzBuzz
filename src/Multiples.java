5d35276ecb35bceccdfe81baa853720d17f3f9b3
Shuxiao <shuxiao_song@outlook.com>
2024-09-12 09:27:32 +0800

    Task 4

---
+++ src/Multiples.java
@@ -0,0 +1,9 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int a = 1000/3;
+        int b = 1000/5;
+        int c = 1000/(3*5);
+        int d = a + b - c;
+        System.out.println(d);
+    }
+}
